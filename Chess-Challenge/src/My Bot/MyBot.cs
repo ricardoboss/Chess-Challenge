a078859cb6c2ed3ddd0b22d95115c294be205e5b
Ricardo Boss <contact@ricardoboss.de>
2023-07-22 00:53:32 +0200

    First attempt at improving EvilBot
    Sort moves randomly, then try to capture the most valuable piece, then try a move that won't get me captured, then choose a random move

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,63 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    Random rng = new();
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        var moves = board.GetLegalMoves().OrderBy(_ => rng.NextDouble()).ToArray();
+
+        return GetMoveThatCaptures(board, moves) ?? GetNotAttackedMove(board, moves) ?? moves[0];
+    }
+
+    Move? GetNotAttackedMove(Board board, Move[] moves)
+    {
+        var allMoves = new Queue<Move>(moves);
+
+        Move moveToPlay;
+        do
+        {
+            if (!allMoves.TryDequeue(out moveToPlay))
+                return null;
+        } while (board.SquareIsAttackedByOpponent(moveToPlay.TargetSquare));
+
+        return moveToPlay;
+    }
+
+    Move? GetMoveThatCaptures(Board board, Move[] moves)
+    {
+        var highestValueCapture = 0;
+        Move? moveToPlay = null;
+        foreach (var move in moves)
+        {
+            if (MoveIsCheckmate(board, move))
+            {
+                moveToPlay = move;
+                break;
+            }
+
+            var capturedPiece = board.GetPiece(move.TargetSquare);
+            if ((int)capturedPiece.PieceType <= highestValueCapture)
+                continue;
+
+            moveToPlay = move;
+            highestValueCapture = (int)capturedPiece.PieceType;
+        }
+
+        if (highestValueCapture == 0) return null;
+
+        return moveToPlay;
+    }
+
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        var isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
     }
 }
