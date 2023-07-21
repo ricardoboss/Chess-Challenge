0fcb010e01607bbd55aa959ad2b05b17096b0b88
Ricardo Boss <contact@ricardoboss.de>
2023-07-21 18:09:35 +0200

    MAke games play 10 times faster

--- Chess-Challenge/src/Framework/Application/Core/ChallengeController.cs
+++ Chess-Challenge/src/Framework/Application/Core/ChallengeController.cs
@@ -228,7 +228,7 @@ namespace ChessChallenge.Application
                 {
                     moveToPlay = chosenMove;
                     isWaitingToPlayMove = true;
-                    const float minDelay = 0.1f;
+                    const float minDelay = 0.01f;
                     playMoveTime = lastMoveMadeTime + minDelay;
                 }
                 else
