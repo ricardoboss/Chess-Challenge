616a52626859ea5e24c36f4319b0be6571ddbaa7
Ricardo Boss <contact@ricardoboss.de>
2023-08-01 01:14:23 +0200

    WIP improvements

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,43 +7,49 @@ using ChessChallenge.API;
 // ReSharper disable All
 public class MyBot : IChessBot
 {
-    const int maxDepth = 3; // should be even for best scoring
+    const int maxDepth = 2; // should be even for best scoring
 
     const int pawnValue = 100;
     const int knightValue = 300;
     const int bishopValue = 300;
-    const int rookValue = 500;
-    const int queenValue = 900;
+    const int rookValue = 600;
+    const int queenValue = 950;
     const int kingValue = 10000;
 
-    const int castlingBonus = 200;
-    const int promotionBonus = 50;
-    const int captureBonus = 100;
-    const int centerBonus = 20;
-    const int pawnPromotionBonus = 1000;
-    const int checkmateBonus = 10000;
-    const int checkBonus = 1000;
+    const int castlingBonus = 300;
+    const int castlingAvailableBonus = 500;
+    const int promotionBonus = 200;
+    const int captureBonus = 500;
+    const int centerBonus = 40;
+    const int checkmateBonus = 100000;
+    const int checkBonus = 500;
+    const int coveredSquareMoveBonus = 5000;
 
-    const int doublePawnPenalty = 30;
-    const int isolatedPawnPenalty = 20;
-    const int edgeKnightPenalty = 10;
-    const int kingMovePenalty = 150;
+    const int doublePawnPenalty = 60;
+    const int isolatedPawnPenalty = 10;
+    const int edgeKnightPenalty = 5;
+    const int kingMovePenalty = 200;
+    const int attackedSquareMovePenalty = 10000;
+    const int revertMovePenalty = 150;
 
-    const int drawPenalty = 1000;
+    const int drawPenalty = 10000;
 
     Board board = null!;
+    Timer timer = null!;
     bool iAmWhite;
+    Move lastMove = Move.NullMove;
 
-    public Move Think(Board b, Timer timer)
+    public Move Think(Board b, Timer t)
     {
         board = b;
+        timer = t;
         iAmWhite = board.IsWhiteToMove;
 
         var (bestScore, bestMove) = GetBestMove(maxDepth);
 
-        Console.WriteLine("Best move score: " + bestScore + ", took " + timer.MillisecondsElapsedThisTurn);
+        Console.WriteLine("Best move score: " + bestScore + ", took " + timer.MillisecondsElapsedThisTurn + "ms");
 
-        return bestMove;
+        return lastMove = bestMove;
     }
 
     (int score, Move move) GetBestMove(int remainingDepth)
@@ -54,7 +60,7 @@ public class MyBot : IChessBot
             if (board.IsDraw() || board.IsInsufficientMaterial())
                 return (drawPenalty, new());
 
-            if (board.IsInCheckmate())
+            if (board.IsInCheck()) // if legal moves are empty, this already means checkmate
                 return (-checkmateBonus, new());
 
             throw new InvalidOperationException("No legal moves, but not in checkmate or draw?");
@@ -86,9 +92,10 @@ public class MyBot : IChessBot
     {
         var score = 0;
 
-        board.MakeMove(move);
+        // pre move evaluation
+        // score += EvaluateBoard();
 
-        var preMoveEvaluation = EvaluateBoard();
+        board.MakeMove(move);
 
         if (remainingDepth > 0)
         {
@@ -97,17 +104,16 @@ public class MyBot : IChessBot
             score -= enemyBestMoveScore;
         }
 
-        var postMoveEvaluation = EvaluateBoard();
+        if (board.SquareIsAttackedByOpponent(move.TargetSquare))
+            score += coveredSquareMoveBonus;
+
+        // post move evaluation
+        score += EvaluateBoard();
 
         board.UndoMove(move);
 
-        score += postMoveEvaluation - preMoveEvaluation;
-
         if (move.IsCastles)
             score += castlingBonus;
-        
-        if (move.IsPromotion)
-            score += pawnPromotionBonus;
 
         if (move.MovePieceType is PieceType.King)
             score -= kingMovePenalty;
@@ -118,16 +124,21 @@ public class MyBot : IChessBot
         if (move.IsPromotion)
             score += promotionBonus + PieceTypeSingleValue(move.PromotionPieceType);
 
+        if (board.SquareIsAttackedByOpponent(move.TargetSquare))
+            score -= attackedSquareMovePenalty;
+
+        if (lastMove.MovePieceType == move.MovePieceType && lastMove.TargetSquare == move.StartSquare)
+            score -= revertMovePenalty;
+
         return score;
     }
-
     int EvaluateBoard()
     {
-        var pawnsValue = PieceTypeValues(PieceType.Pawn, board, iAmWhite) - PieceTypeValues(PieceType.Pawn, board, !iAmWhite);
-        var knightsValue = PieceTypeValues(PieceType.Knight, board, iAmWhite) - PieceTypeValues(PieceType.Knight, board, !iAmWhite);
-        var bishopsValue = PieceTypeValues(PieceType.Bishop, board, iAmWhite) - PieceTypeValues(PieceType.Bishop, board, iAmWhite);
-        var rooksValue = PieceTypeValues(PieceType.Rook, board, iAmWhite) - PieceTypeValues(PieceType.Rook, board, iAmWhite);
-        var queensValue = PieceTypeValues(PieceType.Queen, board, iAmWhite) - PieceTypeValues(PieceType.Queen, board, iAmWhite);
+        var pawnsValue = PieceValuesDiff(PieceType.Pawn, iAmWhite);
+        var knightsValue = PieceValuesDiff(PieceType.Knight, iAmWhite);
+        var bishopsValue = PieceValuesDiff(PieceType.Bishop, iAmWhite);
+        var rooksValue = PieceValuesDiff(PieceType.Rook, iAmWhite);
+        var queensValue = PieceValuesDiff(PieceType.Queen, iAmWhite);
 
         var sum = pawnsValue + knightsValue + bishopsValue + rooksValue + queensValue;
 
@@ -152,49 +163,80 @@ public class MyBot : IChessBot
         PieceType.None => 0,
     };
 
-    static int PieceTypeValues(PieceType type, Board board, bool white)
+    int PieceValuesDiff(PieceType type, bool white) => PieceTypeValues(type, white) - PieceTypeValues(type, !white);
+
+    int PawnValues(bool white)
+    {
+        var pawns = board.GetPieceList(PieceType.Pawn, white);
+        var pawnSum = 0;
+
+        foreach (var pawn in pawns)
+        {
+            pawnSum += pawnValue;
+
+            if (IsInCenterSquares(pawn.Square))
+                pawnSum += centerBonus;
+
+            if (pawns.Any(p => p.Square.File == pawn.Square.File && p.Square.Rank != pawn.Square.Rank))
+                pawnSum -= doublePawnPenalty;
+
+            if (pawns.Any(p => p.Square.File == pawn.Square.File - 1 || p.Square.File == pawn.Square.File + 1))
+                pawnSum -= isolatedPawnPenalty;
+        }
+
+        return pawnSum;
+    }
+
+    int KnightValues(bool white)
+    {
+        var knights = board.GetPieceList(PieceType.Knight, white);
+        var knightsSum = 0;
+
+        foreach (var knight in knights)
+        {
+            knightsSum += knightValue;
+
+            if (IsInCenterSquares(knight.Square))
+                knightsSum += centerBonus;
+
+            if (knight.Square.File is 0 or 7)
+                knightsSum -= edgeKnightPenalty;
+        }
+
+        return knightsSum;
+    }
+
+    int RookValues(bool white)
+    {
+        var rooks = board.GetPieceList(PieceType.Rook, white);
+        var rooksSum = 0;
+
+        foreach (var rook in rooks)
+        {
+            rooksSum += rookValue;
+
+            if (board.HasKingsideCastleRight(white))
+                rooksSum += castlingAvailableBonus;
+
+            if (board.HasQueensideCastleRight(white))
+                rooksSum += castlingAvailableBonus;
+        }
+
+        return rooksSum;
+    }
+
+    int PieceTypeValues(PieceType type, bool white)
     {
         switch (type)
         {
             case PieceType.Pawn:
-                var pawns = board.GetPieceList(type, white);
-                var pawnSum = 0;
-
-                foreach (var pawn in pawns)
-                {
-                    pawnSum += pawnValue;
-
-                    if (IsInCenterSquares(pawn.Square))
-                        pawnSum += centerBonus;
-
-                    if (pawns.Any(p => p.Square.File == pawn.Square.File && p.Square.Rank != pawn.Square.Rank))
-                        pawnSum -= doublePawnPenalty;
-
-                    if (pawns.Any(p => p.Square.File == pawn.Square.File - 1 || p.Square.File == pawn.Square.File + 1))
-                        pawnSum -= isolatedPawnPenalty;
-                }
-
-                return pawnSum;
+                return PawnValues(white);
             case PieceType.Knight:
-                var knights = board.GetPieceList(type, white);
-                var knightsSum = 0;
-
-                foreach (var knight in knights)
-                {
-                    knightsSum += knightValue;
-
-                    if (IsInCenterSquares(knight.Square))
-                        knightsSum += centerBonus;
-
-                    if (knight.Square.File is 0 or 7)
-                        knightsSum -= edgeKnightPenalty;
-                }
-
-                return knightsSum;
+                return KnightValues(white);
             case PieceType.Bishop:
                 return board.GetPieceList(type, white).Count * bishopValue;
             case PieceType.Rook:
-                return board.GetPieceList(type, white).Count * rookValue;
+                return RookValues(white);
             case PieceType.Queen:
                 return board.GetPieceList(type, white).Count * queenValue;
             case PieceType.King:
